21888487984ebb22635c21d39cc1813bedf78f69
Sam Handler <sam.handler@shopify.com>
2022-12-20 15:42:58 -0700

    make logger work

--- lib/opentelemetry/api/utils/logger.lua
+++ lib/opentelemetry/api/utils/logger.lua
@@ -8,43 +8,43 @@ function _M.new(log_level)
     return setmetatable({ log_level = log_levels[log_level] or log_levels.error }, mt)
 end
 
-function _M.write(self, message, callsite_level)
-    if self.log_level > callsite_level then
+local function write(message, instance_level, callsite_level)
+    if instance_level > callsite_level then
         return
     end
-    print(message)
+    print("OpenTelemetry: " .. message)
 end
 
-function _M.debug(self, message)
-    self:write(message, log_levels.debug)
+function _M:debug(message)
+    write(message, self.log_level, log_levels.debug)
 end
 
-function _M.info(self, message)
-    self:write(message, log_levels.info)
+function _M:info(message)
+    write(message, self.log_level, log_levels.info)
 end
 
-function _M.notice(self, message)
-    self:write(message, log_levels.notice)
+function _M:notice(message)
+    write(message, self.log_level, log_levels.notice)
 end
 
-function _M.warn(self, message)
-    self:write(message, log_levels.warn)
+function _M:warn(message)
+    write(message, self.log_level, log_levels.warn)
 end
 
-function _M.error(self, message)
-    self:write(message, log_levels.error)
+function _M:error(message)
+    write(message, self.log_level, log_levels.error)
 end
 
-function _M.crit(self, message)
-    self:write(message, log_levels.crit)
+function _M:crit(message)
+    write(message, self.log_level, log_levels.crit)
 end
 
-function _M.alert(self, message)
-    self:write(message, log_levels.alert)
+function _M:alert(message)
+    write(message, self.log_level, log_levels.alert)
 end
 
-function _M.emerg(self, message)
-    self:write(message, log_levels.emerg)
+function _M:emerg(message)
+    write(message, self.log_level, log_levels.emerg)
 end
 
 return _M
