bed037fef446b16d74dd33598b08be3a1750a114
Sam Handler <sam.handler@shopify.com>
2022-12-21 11:25:22 -0700

    update logger docs

--- lib/opentelemetry/api/utils/logger.lua
+++ lib/opentelemetry/api/utils/logger.lua
@@ -1,13 +1,24 @@
+--- A simple logger that uses print(). SDK loggers should adhere to the same interface.
+-- @module logger
+
 local _M = {}
 
 local mt = { __index = _M }
 
 local log_levels = { debug = 0, info = 1, notice = 2, warn = 3, error = 4, crit = 5, alert = 6, emerg = 7 }
 
+--- Return a new logger instance
+-- @param[type=string] log_level The log level to use. Defaults to 'error'.
+-- @return A new logger instance
 function _M.new(log_level)
     return setmetatable({ log_level = log_levels[log_level] or log_levels.error }, mt)
 end
 
+--- Write message to stdout
+-- @param[type=string] message The message to write.
+-- @param[type=number] instance_level The log level of the logger instance.
+-- @param[type=number] callsite_level The log level at which the message was logged.
+-- @return nil
 local function write(message, instance_level, callsite_level)
     if instance_level > callsite_level then
         return
@@ -15,34 +26,58 @@ local function write(message, instance_level, callsite_level)
     print("OpenTelemetry: " .. message)
 end
 
+--- Write debug message
+-- @param[type=string] message The message to write.
+-- @return nil
 function _M:debug(message)
     write(message, self.log_level, log_levels.debug)
 end
 
+--- Write info message
+-- @param[type=string] message The message to write.
+-- @return nil
 function _M:info(message)
     write(message, self.log_level, log_levels.info)
 end
 
+--- Write notice message
+-- @param[type=string] message The message to write.
+-- @return nil
 function _M:notice(message)
     write(message, self.log_level, log_levels.notice)
 end
 
+--- Write warn message
+-- @param[type=string] message The message to write.
+-- @return nil
 function _M:warn(message)
     write(message, self.log_level, log_levels.warn)
 end
 
+--- Write error message
+-- @param[type=string] message The message to write.
+-- @return nil
 function _M:error(message)
     write(message, self.log_level, log_levels.error)
 end
 
+--- Write crit message
+-- @param[type=string] message The message to write.
+-- @return nil
 function _M:crit(message)
     write(message, self.log_level, log_levels.crit)
 end
 
+--- Write alert message
+-- @param[type=string] message The message to write.
+-- @return nil
 function _M:alert(message)
     write(message, self.log_level, log_levels.alert)
 end
 
+--- Write emerg message
+-- @param[type=string] message The message to write.
+-- @return nil
 function _M:emerg(message)
     write(message, self.log_level, log_levels.emerg)
 end
